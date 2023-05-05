744e55a8f2c8eee943af1e36b46d5c401944854d
Hitesh Gami <hiteshg@universal-software.com>
2023-05-05 16:50:45 +0530

    roll

---
+++ stk_roll/stk_roll/doctype/roll_stock/roll_stock.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, gami and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Roll Stock', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ stk_roll/stk_roll/doctype/roll_stock/test_roll_stock.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, gami and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRollStock(FrappeTestCase):
+	pass
