52214976407b213955054256521df3263f0b839d
Hitesh Gami <gamihitesh@gmail.com>
2023-05-06 10:12:49 +0530

    fix

--- stk_roll/stk_roll/doctype/roll_stock/roll_stock.js
+++ stk_roll/stk_roll/doctype/roll_stock/roll_stock.js
@@ -2,9 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Roll Stock', {
-	// refresh: function(frm) {
-
-	// }
 	onload: function (frm) {
 		frm.set_query('item', () => {
 			return {
@@ -12,7 +9,38 @@ frappe.ui.form.on('Roll Stock', {
 					'has_variants': true
 				}
 			}
+		});
+		frappe.call({
+			method: 'stk_roll.stk_roll.doctype.roll_stock.roll_stock.get_attribute_query',
+			args: {
+				parent: 'Quality'
+			},
+			 
+			callback: (r) => {
+				frm.set_df_property('quality', 'options', r.message)
+			},
 		})
-
+		frappe.call({
+			method: 'stk_roll.stk_roll.doctype.roll_stock.roll_stock.get_attribute_query',
+			args: {
+				parent: 'Colour'
+			},
+			 
+			callback: (r) => {
+				frm.set_df_property('colour', 'options', r.message)
+			},
+		})
+		frappe.call({
+			method: 'stk_roll.stk_roll.doctype.roll_stock.roll_stock.get_attribute_query',
+			args: {
+				parent: 'GSM'
+			},
+			 
+			callback: (r) => {
+				frm.set_df_property('gsm', 'options', r.message)
+			},
+		})
+		
 	},
+
 });
--- stk_roll/stk_roll/doctype/roll_stock/roll_stock.py
+++ stk_roll/stk_roll/doctype/roll_stock/roll_stock.py
@@ -1,8 +1,18 @@
 # Copyright (c) 2023, gami and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RollStock(Document):
 	pass
+
+
+@frappe.whitelist()
+def get_attribute_query(parent):
+	return frappe.db.sql(
+		"""select attribute_value from `tabItem Attribute Value` 
+			where parent = %s""",
+		(parent),
+		 
+	)
