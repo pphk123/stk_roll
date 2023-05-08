5c6277568f6e65083b01d2913e952a49f666f1b3
Hitesh Gami <gamihitesh@gmail.com>
2023-05-08 10:29:53 +0530

    add lm roll

---
+++ stk_roll/stk_roll/doctype/lamination_roll/lamination_roll.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, gami and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class LaminationRoll(Document):
+	pass
