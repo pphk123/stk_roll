cc8500cf76cb56212f32aa2942f38b468b8c535b
Hitesh Gami <gamihitesh@gmail.com>
2023-05-13 20:33:08 +0530

    fix

--- stk_roll/stk_roll/doctype/lamination_roll/lamination_roll.py
+++ stk_roll/stk_roll/doctype/lamination_roll/lamination_roll.py
@@ -7,11 +7,15 @@ from frappe.model.document import Document
 
 class LaminationRoll(Document):
     def before_submit(self):
-        ul_roll_doc = frappe.get_doc(
-            'Roll Stock', self.ul_roll)
-        if ul_roll_doc.status == 'Active':
-            ul_roll_doc.status = 'Empty'
+        ul_roll_doc = frappe.get_doc("Roll Stock", self.ul_roll)
+        if ul_roll_doc.status == "Active":
+            ul_roll_doc.status = "Empty"
             ul_roll_doc.save()
         else:
-            frappe.throw(
-                "<b>{0}</b> is not Active".format(ul_roll_doc.name))
+            frappe.throw("<b>{0}</b> is not Active".format(ul_roll_doc.name))
+
+    def on_cancel(self):
+        ul_roll_doc = frappe.get_doc("Roll Stock", self.ul_roll)
+        if ul_roll_doc.status == "Empty":
+            ul_roll_doc.status = "Active"
+            ul_roll_doc.save()
